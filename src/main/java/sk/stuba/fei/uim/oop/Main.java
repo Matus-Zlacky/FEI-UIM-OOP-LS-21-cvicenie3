f4799ab5938b237bee5ae715a98e4d0dab018427
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-01 13:50:03 +0100

    metody

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,15 +5,15 @@ import sk.stuba.fei.uim.oop.car.Auto;
 public class Main {
     public static void main(String[] args) {
         Auto skoda = new Auto();
-        Auto bmw = new Auto();
 
-        skoda.one = 1;
-        bmw.one = 2;
+        skoda.stavNadrze = 100;
 
-        if(skoda.one == bmw.one) {
-            System.out.println("rovnake");
-        } else {
-            System.out.println("rozne");
-        }
+        skoda.drive(100);
+
+        System.out.println(skoda.stavNadrze);
+
+        skoda.drive(200);
+
+        System.out.println(skoda.stavNadrze);
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/car/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/car/Auto.java
@@ -1,13 +1,15 @@
 package sk.stuba.fei.uim.oop.car;
 
 public class Auto {
-    public int pocetKolies;
-    public int pocetDvery;
-    public double kapacitaNadrze;
-    public String nazov;
+    private static final double FUEL_PER_KM = 0.5;
 
-    public int one;
-    int two;
-    protected int three;
-    private int four;
+    public double kapacitaNadrze;
+    public double stavNadrze;
+
+    public void drive(double distanceInKm) {
+        stavNadrze -= distanceInKm * FUEL_PER_KM;
+        if (stavNadrze < 0) {
+            stavNadrze = 0;
+        }
+    }
 }
