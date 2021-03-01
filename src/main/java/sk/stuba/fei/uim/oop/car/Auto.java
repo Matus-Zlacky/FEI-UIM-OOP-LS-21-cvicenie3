74c9ff5bee634db8cac66e10d9bf906a26ab6d9e
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-01 13:54:29 +0100

    getter/setter

--- src/main/java/sk/stuba/fei/uim/oop/car/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/car/Auto.java
@@ -3,13 +3,21 @@ package sk.stuba.fei.uim.oop.car;
 public class Auto {
     private static final double FUEL_PER_KM = 0.5;
 
-    public double kapacitaNadrze;
-    public double stavNadrze;
+    private double kapacitaNadrze;
+    private double stavNadrze;
 
     public void drive(double distanceInKm) {
         stavNadrze -= distanceInKm * FUEL_PER_KM;
         if (stavNadrze < 0) {
             stavNadrze = 0;
         }
+    }
+
+    public double getStavNadrze() {
+        return stavNadrze;
+    }
+
+    public void setStavNadrze(double stavNadrze) {
+        this.stavNadrze = stavNadrze;
     }
 }
