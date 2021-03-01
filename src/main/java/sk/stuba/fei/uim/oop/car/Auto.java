ed61ce5a245d0fc7a2c404b6d2b9d67d3a2c5350
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-01 13:42:18 +0100

    modifikatory pristupu

---
+++ src/main/java/sk/stuba/fei/uim/oop/car/Auto.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.car;
+
+public class Auto {
+    public int pocetKolies;
+    public int pocetDvery;
+    public double kapacitaNadrze;
+    public String nazov;
+
+    public int one;
+    int two;
+    protected int three;
+    private int four;
+}
